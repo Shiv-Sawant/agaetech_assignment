b984fe6dc36534f4825ecdeb5dc35b2b85b9093b
Shiv-Sawant <shivsawant@1958@gmail.com>
2024-09-01 22:26:41 +0530

    updated css

--- src/component/PostContents.tsx
+++ src/component/PostContents.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import { defaultPosts } from "../utils/constants"
 
 interface IPostContents {
     datas: boolean
@@ -7,106 +8,44 @@ interface IPostContents {
 const PostContents = ({ datas }: IPostContents) => {
     const getLocalPosts = localStorage.getItem('posts')
     const parseData = getLocalPosts && JSON.parse(getLocalPosts) || []
-
     useEffect(() => {
-
+        const element = document.querySelectorAll('#common-sub-header')
+        if (element) {
+            element.forEach(ele => {
+            ele.style.color = 'rgba(24, 133, 255, 255)'
+                
+            });
+        }
     }, [datas])
 
     return (
         <>
-            <div className="post-section-content">
-                <div className="post-content-head">
-                    <h1 className="post-content-head-title">Sample blog post</h1>
-                    <p>Januvary 1, 2014 by<span className="common-sub-header"> Mark </span></p>
-                </div>
-                <div className="post-content-body">
-                    <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-                   <div className="breakline"></div>
-                    <p>
-                        Cum sociis natoque penatibus et magnis <span className="common-sub-header">dis parturient montes</span>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
-                    </p>
-                    <p>
-                        Curabitur blandit tempus porttitor. <b>Nullam quis risus eget urna mollis</b> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit
-                    </p>
-                    <p>
-                        Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla and consectetur.
-                    </p>
-                    <h1>Heading</h1>
-                    <p>
-                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                    </p>
-                    <h1>Sub-heading</h1>
-                    <p>
-                        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                    </p>
-                    <p>Example code block</p>
-                    <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                    <h1>Sub-heading</h1>
-                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <ul>
-                        <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                        <li>Donec id elit non mi porta gravida at eget metus.</li>
-                        <li>Nulla vitae elit libero, a pharetra augue.</li>
-                    </ul>
-                    <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                    <ol>
-                        <li>Vestibulum id ligula porta felis euismod semper.</li>
-                        <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                        <li>Maecenes sed diam eget risus varius blandit sit amet non magna.</li>
-                    </ol>
-                    <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis</p>
-                </div>
-            </div>
-
-            <div className="post-section-content">
-                <div className="post-content-head">
-                    <h1 className="post-content-head-title">Another blog post</h1>
-                    <p>December 23, 2012 by <span className="common-sub-header">Jacob</span></p>
-                </div>
-                <div className="post-content-body">
-                    <p>
-                        Cum sociis natoque penatibus et magnis <span className="common-sub-header">dis parturient montes</span>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
-                    </p>
-                    <p>
-                        Curabitur blandit tempus porttitor. <b>Nullam quis risus eget urna mollis</b> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit
-                    </p>
-                    <p>
-                        Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla and consectetur.
-                    </p>
-                    <p>
-                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                    </p>
-                </div>
-            </div>
-
-            <div className="post-section-content">
-                <div className="post-content-head">
-                    <h1 className="post-content-head-title">New feature</h1>
-                    <p>December 14, 2013 by <span className="common-sub-header">Chirs</span></p>
-                </div>
-
-                <div className="post-content-body">
-                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <ul>
-                        <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                        <li>Donec id elit non mi porta gravida at eget metus.</li>
-                        <li>Nulla vitae elit libero, a pharetra augue.</li>
-                    </ul>
-                    <p>
-                        Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla and consectetur.
-                    </p>
-                    <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                </div>
-
-            </div>
-
+            {
+                defaultPosts.map((posts) => {
+                    return (
+                        <div className="post-section-content">
+                            <div className="post-content-head">
+                                <h1 className="post-content-head-title">{posts.title}</h1>
+                                <p> {posts.date} <span className="common-sub-header"> {posts.by} </span></p>
+                            </div>
+                            <div className="post-content-body">
+                                {/* {posts.content[0]} */}
+                                {posts.content.map((data, index) => {
+                                    return (
+                                        <div key={index} dangerouslySetInnerHTML={{ __html: data }} />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })
+            }
             {
                 parseData.length != 0 && parseData.map((data: any, index: any) => {
                     return (
                         <div className="post-section-content" key={index}>
                             <div className="post-content-head">
                                 <h1 className="post-content-head-title">{data.PostTitle}</h1>
-                                {/* <p>December 14, 2013 by <span className="common-sub-header">{data.PostBy}</span></p> */}
                                 <p>{data.PostDate} by <span className="common-sub-header">{data.PostBy}</span></p>
                             </div>
                             <div className="post-content-body">
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -48,3 +48,83 @@ export const posts = [
     }
 ]
 
+export const defaultPosts = [
+    {
+        by: "Mark",
+        title: "Sample blog post",
+        date: "Januvary 1, 2014 by",
+        content: [
+            `<p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>`,
+            `<p> <p className="breakline"></p></p>`,
+            `  <p>
+                     <p>   Cum sociis natoque penatibus et magnis <b  id='common-sub-header'>dis parturient montes</b>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+                    </p>`,
+            ` <p>
+                        Curabitur blandit tempus porttitor. <b>Nullam quis risus eget urna mollis</b> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit
+                    </p>`,
+            ` <p>
+                        Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla and consectetur.
+                    </p>`,
+            `<p><h1>Heading</h1></p>`,
+            ` <p>
+                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                    </p>`,
+            `<p> <h1>Sub-heading</h1></p>`,
+            ` <p>
+                        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                    </p>`,
+            `<p>Example code block</p>`,
+            `<p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>`,
+            ` <h1>Sub-heading</h1>`,
+            `<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>`,
+            `<p> <ul>
+                        <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+                        <li>Donec id elit non mi porta gravida at eget metus.</li>
+                        <li>Nulla vitae elit libero, a pharetra augue.</li>
+                    </ul></p>`,
+            ` <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>`,
+            `<p><ol>
+                        <li>Vestibulum id ligula porta felis euismod semper.</li>
+                        <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
+                        <li>Maecenes sed diam eget risus varius blandit sit amet non magna.</li>
+                    </ol></p>`,
+            `<p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis</p>`,
+        ],
+    },
+    {
+        by: "Jacob",
+        title: "Another blog post",
+        date: "December 23, 2012 by",
+        content: [
+            `<p>
+                        Cum sociis natoque penatibus et magnis <span id="common-sub-header">dis parturient montes</span>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+                    </p>`,
+                    `<p>
+                        Curabitur blandit tempus porttitor. <b>Nullam quis risus eget urna mollis</b> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit
+                    </p>`,
+                    ` <p>
+                        Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla and consectetur.
+                    </p>`,
+                    `<p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>`,
+                    
+        ]
+    },
+    {
+        by: "Chirs",
+        title: "New feature",
+        date: "December 14, 2013 by",
+        content: [
+            `<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>`,
+            `<p><ul>
+                        <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+                        <li>Donec id elit non mi porta gravida at eget metus.</li>
+                        <li>Nulla vitae elit libero, a pharetra augue.</li>
+                    </ul></p>`,
+            `<p>
+                        Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla and consectetur.
+                    </p>`,
+            `<p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>`,
+        ] 
+    }
+]
+
